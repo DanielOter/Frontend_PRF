436af79407ee785191560d2c9d3c5017ec842fc5
kenancha <kenanchab@gmail.com>
2022-11-21 20:41:40 -0300

    Marcadores y mapeo

--- components/user/MapScreen.js
+++ components/user/MapScreen.js
@@ -3,22 +3,37 @@ import MapView, {Marker} from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
  function MapScreen() {
+
+const m = [{latitude: -34.584510, 
+  longitude: -58.416899,
+  title:"test",
+  description:'aca',
+  pinColor:'gold'},
+]
+
+
   return (
     <View style={styles.container}>
       <MapView style={styles.map} 
           initialRegion={{
-            latitude: -34.5831892,
+            latitude: -34.5831892,// ubicacion del barrio
             longitude: -58.41367,
             latitudeDelta: 0.0422,
             longitudeDelta: 0.0221,
           }}
+          showsUserLocation={true}
         >
-                  <Marker
-          coordinate={{latitude: -34.581290,longitude: -58.420546}}
-          title="test"
-          description='aca'
-          pinColor='gold'
-          />
+          {/*alertas*/}
+          {m.map((item) => ( 
+              <Marker
+              coordinate={{latitude: item.latitude,longitude: item.longitude}}
+              title={item.title}
+              description={item.description}
+              pinColor='red'
+              />
+
+
+          ))}
 
         </MapView>
 
